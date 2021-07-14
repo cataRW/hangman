a98b6d67e34e9c7be1ed7e4fc81f3f8cffd65917
cataRW <86371141+cataRW@users.noreply.github.com>
2021-07-14 08:44:19 -0700

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -1,93 +1,91 @@
-//i dont't know if it's a good practice what i have done here, please let me know Petru
-let allWords = ['matemathic', 'mate', 'friends', 'wellcode', 'youtube', 'facebook', 'function', 'zuckerberg', 'selector', 'entertaining', 'funny', 'united', 'translate'];
+let allWords = ['matemathic', 'mate', 'friends', 'wellcode', 'youtube', 'facebook', 'function', 'zuckerberg', 'selector', 'entertaining', 'funny', 'united', 'translate', 'work', 'house', 'crypto', 'then', 'and', 'random', 'customize', 'sleep', 'drink', 'food', 'run'];
 let rounds = 1;
 let wrongs, randomWord;
 
-function startGame() {
+function visibilitySetter() {
+  document.getElementById('startGame').style.visibility = 'hidden';
+  document.querySelector('.restart-container').style.visibility = 'hidden';
+  document.querySelector('.status').style.visibility = 'visible';
 
-  // i do that for the moment when the user restart the game
-  wrongs = 0;
-  randomWord = allWords[Math.floor(Math.random() * allWords.length)];
-
-  // here I played a little bit with visibility
   for(let i = 0; i < 10; ++i) {
-    $('#' + (100 + i)).css('visibility', 'hidden');
-    if(i === 0) {
-      $('#' + (100 + i)).css('visibility', 'visible');
-    }
+    document.getElementById(100 + i).style.visibility = 'hidden';
   }
-  $(".wrongs").html(wrongs);
-  $('.rounds').html(rounds);
-  $("#startGame").hide();
-  $('.status').css('visibility', 'visible');
-  $('.restart-container').css('visibility', 'hidden');
-  $('.finalMessage').html('!!!YOU WON, CONGRATS!!!');
+}
 
-// after the 'game start' when button was clicked i generate the game elements (like input, '_ _ _ _')
-  $('.input-output-container').append('<div class="inputLetters"></div>');
+function letterChangeInRandomWord(letter) {
+  let letterIndex = randomWord.indexOf(letter);
+
+  if(letterIndex === 0) {
+    randomWord = '_' + randomWord.substr(letterIndex + 1);
+  }else if(letterIndex === randomWord.length - 1) {
+    randomWord = randomWord.substr(0, letterIndex) + '_';
+  }else {
+    randomWord = randomWord.substr(0, letterIndex) + '_' + randomWord.substr(letterIndex + 1);
+  }
+}
+
+function inputOutputCreator(firstLetter) {
+  $('body').append('<div class="input-output-container"></div>');
+  $('.input-output-container').append('<div class ="inputLetters"></div>').append('<label for="userInput" class="userInputLabel">Add your letter here: </label>').append('<input id="userInput" type="text" placeholder="here" value="" autofocus />').append('<input id="submitInput" type="submit" value="submit" onclick="inputCheck()"/>');
   $('.input-output-container').append('<div class="outputLetters"></div>');
-  $('.restart-container').css('visibility', 'hidden');
-  $('.inputLetters').append('<label for="userInput" class="label">Add your letter here: </label>').append('<input id="userInput" type="text" placeholder="here" value="" autofocus />').append('<input id="submitInput" type="submit" value="submit" onclick="validInput()"/>');
 
   for(let i = 0; i < randomWord.length; ++i) {
-    if(randomWord.charAt(0) === randomWord.charAt(i)) {
-      $('.outputLetters').append('<div class="word" id="'+ i +'"> ' + randomWord.charAt(0) + ' </div>');
-    } else {
+    if(randomWord[i] === firstLetter) {
+      $('.outputLetters').append('<div class="word" id="'+ i +'"> ' + firstLetter + ' </div>');
+      letterChangeInRandomWord(firstLetter);
+    }else {
       $('.outputLetters').append('<div class="word" id="' + i + '"> _ </div>');
     }
   }
-  randomWord = '_' + randomWord.substr(1); //if i hadn't done this the endGame function it will never be called if the player win
 }
 
-//here i wanna to be sure that the input it is a single letter
-function validInput() {
-  let input = $('#userInput').val();
-  input = input.toLowerCase();
-  if(input.length === 1 && ('a' <= input && input <= 'z')) {
-    $('.label').text('Add your letter here: ');
-    gameStateCheck();
-  } else if('a' > input || input > 'z') {
-    $('.label').text('You must insert a letter!');
-  } else {
-    $('.label').text('You must insert just one letter');
+function startGame() {
+  wrongs = 0;
+  randomWord = allWords[Math.floor(Math.random() * allWords.length)];
+
+  document.querySelector('.wrongs').innerText = wrongs;
+  document.querySelector('.rounds').innerText = rounds;
+  document.querySelector('.finalMessage').innerText = '!!!YOU WON, CONGRATS!!!';
+
+  visibilitySetter();
+  inputOutputCreator(randomWord[wrongs]);
+}
+
+function inputCheck() {
+  const input = document.getElementById('userInput').value.toLowerCase();
+  if(input.length === 1 && /[a-z]/i.test(input)) {
+    document.querySelector('.userInputLabel').innerText = 'Add your letter here: ';
+    gameStateCheck(input);
+  }else {
+    document.querySelector('.userInputLabel').innerText = 'Please, a letter and just one letter!';
   }
 }
 
-//here i checked input that i get(and i have 3 cases)
-function gameStateCheck() {
-  const input = $('#userInput').val();
+function gameStateCheck(input) {
   let index = randomWord.indexOf(input);
-  let fleg = 0
+  const message = document.querySelector('.finalMessage');
 
-  while(index !== -1) {
-    $('#' + index).html(input);
-    randomWord = randomWord.substr(0, index) + '_' + randomWord.substr(index + 1);
-    index = randomWord.indexOf(input);
-    ++fleg;
-  }
-
-  if(fleg === 0) {
-    ++wrongs;
-    $('.wrongs').html(wrongs);
-    $('#' + (100 + wrongs)).css('visibility', 'visible');
-    if(wrongs === 9) {
-      $('.finalMessage').html('!!!YOU LOSE, TRY AGAIN!!!');
-      endGame($('.finalMessage').val());
+  if(index !== -1) {
+    while(index !== -1) {
+      document.getElementById(index).innerText = input;
+      letterChangeInRandomWord(input);
+      index = randomWord.indexOf(input);
+    }
+    if(/[a-z]/i.test(randomWord) == false) {
+      endGame(message.innerText);
     }
   }else {
-    for(let i = 0; i < randomWord.length; ++i) {
-      if(randomWord[i] !== '_') {
-        break;
-      } else if(i === (randomWord.length - 1)) {
-        endGame($('.finalMessage').val());
-      }
+    document.getElementById(100 + wrongs).style.visibility = 'visible';
+    document.querySelector('.wrongs').innerText = ++wrongs;
+    if(wrongs == 10) {
+      message.innerText = '!!!YOU LOSE, TRY AGAIN!!!';
+      endGame(message.innerText);
     }
   }
 }
 
 function endGame(message) {
+  document.querySelector('.input-output-container').remove();
+  document.querySelector('.restart-container').style.visibility = 'visible';
   ++rounds;
-  $('.inputLetters').remove();
-  $('.outputLetters').remove();
-  $('.restart-container').css('visibility', 'visible');
 }
